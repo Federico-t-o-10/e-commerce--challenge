fb668fdb6d38911dbfcc69cf7d520b435a0e40aa
Luis Federico Torres Olivera <luisfederico.t.o.10@gmail.com>
2023-12-03 21:10:37 -0300

    crear app

---
+++ e-commerce/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Home from "../src/page/Home"
+import Header from './components/Header/Header'
+const App = () => {
+  return (
+    <div id="root">
+        <Header />
+        <Home />
+    </div>
+  )
+}
+
+export default App
---
+++ e-commerce/src/components/Header/Header.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Logo from "../../assets/img/icono";
+
+const Header = () => {
+  return (
+    <div>
+      <header className="flex">
+        <div className="flex">
+          <button>holas</button>
+        </div>
+
+        <div>
+          <button>cargas</button>
+        </div>
+      </header>
+    </div>
+  );
+};
+
+export default Header;
